bceb35b39d89c8a4fb836cead70150680c594d83
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2020-01-14 23:18:40 +0530

    create winit window

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        virtual_keycode,
+                        state,
+                        ..
+                    } => match (virtual_keycode, state) {
+                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                            *control_flow = ControlFlow::Exit
+                        }
+
+                        _ => (),
+                    },
+                },
+
+                _ => (),
+            },
+
+            Event::MainEventsCleared => window.request_redraw(),
+
+            // todo draw frame on this
+            Event::RedrawRequested(_window_id) => (),
+
+            _ => (),
+        }
+    });
+}
